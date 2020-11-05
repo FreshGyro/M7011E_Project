0a1f5669cab796e57bf53f7b8977798d79f0ea2a
Amadeus Johansson <maxibrillion@gmail.com>
2020-11-05 14:10:58 +0100

    GetWindSpeed

---
+++ simulator/weather.js
@@ -0,0 +1,7 @@
+const perlin = require("./perlin.js");
+
+function getWindSpeed(x, y) {
+	const time = new Date().getTime() * 10;
+	return perlin(1337, 8, 10000, x, y, time);
+}
+module.exports.getWindSpeed = getWindSpeed;
