cded9407c8ce3bedf45039f6720cebe1d59a4135
Jonathan <25746136+ojaebe-6@users.noreply.github.com>
2020-11-25 15:30:22 +0100

    Nicer prosumer info interface

---
+++ prosumer_interface/info.js
@@ -0,0 +1,29 @@
+
+(function() {
+	const info = document.getElementById("info");
+	const consumptionInfo = info.getElementsByTagName("p")[0];
+	const batteryInfo = info.getElementsByTagName("p")[1];
+	const productionInfo = info.getElementsByTagName("p")[2];
+	const marketRatioSlider = document.getElementById("marketRatio");
+
+	function updateInfo() {
+		const request = new XMLHttpRequest();
+		request.onreadystatechange = function() {
+			if(this.readyState == 4 && this.status == 200) {
+				const json = JSON.parse(request.responseText);
+				if(json.hasOwnProperty("error")) {
+					console.error("Error: " + json["error"]);
+				} else {
+					consumptionInfo.textContent = Math.floor(json["consumption"]) + " W";
+					batteryInfo.textContent = Math.floor(json["battery"]) + " J / " + Math.floor(json["max_battery"]) + " J";
+					productionInfo.textContent = Math.floor(json["wind"] * 100) + "% | " + Math.floor(json["production"]) + " W";
+					marketRatioSlider.value = json["market_ratio"] * 100;
+				}
+			}
+		};
+		request.open("POST", "http://127.0.0.1:81/getprosumerdata", true);
+		request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		request.send("username=" + account.getUsername() + "&password=" + account.getPasswordHash());
+	}
+	setInterval(updateInfo, 1000);
+})();
--- prosumer_interface/main.js
+++ prosumer_interface/main.js
@@ -1,52 +1,7 @@
 
-const info = document.getElementById("info");
 const marketRatioSlider = document.getElementById("marketRatio");
 const imageUpload = document.getElementById("imageUpload");
 const image = document.getElementById("image");
-
-function updateInfo() {
-	const request = new XMLHttpRequest();
-	request.onreadystatechange = function() {
-		if(this.readyState == 4 && this.status == 200) {
-			info.innerHTML = "";
-			const json = JSON.parse(request.responseText);
-			if(json.hasOwnProperty("error")) {
-				info.innerHTML = "Error: " + json["error"];
-			} else {
-				const keys = [
-					"Wind",
-					"Production",
-					"Consumption",
-					"Delta",
-					"Battery",
-					"Max Battery",
-					"Market Ratio"
-				];
-				const values = [
-					json["wind"],
-					json["production"],
-					json["consumption"],
-					json["production"] - json["consumption"],
-					json["battery"],
-					json["max_battery"],
-					json["market_ratio"]
-				];
-
-				for(let i = 0; i < keys.length; ++i) {
-					const p = document.createElement("p");
-					p.innerText = keys[i] + ": " + values[i];
-					info.appendChild(p);
-				}
-
-				marketRatioSlider.value = json["market_ratio"] * 100;
-			}
-		}
-	};
-	request.open("POST", "http://127.0.0.1:81/getprosumerdata", true);
-	request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	request.send("username=" + account.getUsername() + "&password=" + account.getPasswordHash());
-}
-setInterval(updateInfo, 1000);
 
 marketRatioSlider.onchange = function() {
 	const request = new XMLHttpRequest();
