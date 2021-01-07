f7ed277b017b300b75980cc126d028ed328668ae
Jonathan <25746136+ojaebe-6@users.noreply.github.com>
2021-01-07 12:17:13 +0100

    Show prosumer data in manager table

--- manager_interface/prosumer_list.js
+++ manager_interface/prosumer_list.js
@@ -8,26 +8,46 @@
 			if(this.readyState == 4 && this.status == 200) {
 				const json = JSON.parse(request.responseText);
 
-				table.innerHTML = "<tr><th>Username</th><th>ID</th><th>Status</th></tr>";
+				table.innerHTML = "<tr><th>Username</th><th>ID</th><th>Status</th><th>Battery</th><th>Max battery</th><th>Consumption</th><th>Production</th><th>Wind</th></tr>";
 				for(let i = 0; i < json.length; ++i) {
 					const user = json[i];
 
 					const tr = document.createElement("tr");
 
 					let td = document.createElement("td");
-					td.textContent = user.username;
+					td.textContent = user["username"];
 					tr.appendChild(td);
 
 					td = document.createElement("td");
-					td.textContent = user.id;
+					td.textContent = user["id"];
 					tr.appendChild(td);
 
 					td = document.createElement("td");
-					if(user.isActive) {
+					if(user["isActive"]) {
 						td.textContent = "LOGGED IN";
 					} else {
 						td.textContent = "LOGGED OUT";
 					}
+					tr.appendChild(td);
+
+					td = document.createElement("td");
+					td.textContent = Math.floor(user["battery"]) + " J";
+					tr.appendChild(td);
+
+					td = document.createElement("td");
+					td.textContent = Math.floor(user["max_battery"]) + "J";
+					tr.appendChild(td);
+
+					td = document.createElement("td");
+					td.textContent = Math.floor(user["consumption"]) + " W";
+					tr.appendChild(td);
+
+					td = document.createElement("td");
+					td.textContent = Math.floor(user["production"]) + " W";
+					tr.appendChild(td);
+
+					td = document.createElement("td");
+					td.textContent = Math.floor(user["wind"] * 100) + "%";
 					tr.appendChild(td);
 
 					table.appendChild(tr);
